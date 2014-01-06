ed27db7e115a1e916a61de15b01a936fd90a164e
victorfei <vjhfei@gmail.com>
2014-01-06 15:42:12 -0800

    commented out a line for heroku

--- arduino_django_site/settings.py
+++ arduino_django_site/settings.py
@@ -125,7 +125,7 @@ LOGIN_REDIRECT_URL = '/profile'
 
 # ---------------Heroku-----------------
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+#DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
