1c53bd415ebae0511cc58a26a356d0ad0be622c8
victorfei <vjhfei@gmail.com>
2014-01-06 11:15:18 -0800

    modified landing page for heroku testing

--- arduino_django_site/views.py
+++ arduino_django_site/views.py
@@ -4,11 +4,21 @@ General views for arduino_django project
 Author(s):
     Victor Fei
 """
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.template.loader import get_template
 from django.shortcuts import render
 from django.template import Context, RequestContext
 
 def landing(request):
     """ Links to the landing page, where the user can also request invite """
-    return render(request, 'landing.html', { 'request' : request })
+#    if request:
+#        text = "GET request got"
+    text = request.scheme
+
+    print text
+    print "hello"
+
+    return render(request, 'landing.html', {
+        'request' : request,
+        'text': text,
+    })
